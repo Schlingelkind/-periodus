f2c33fe7073cd0d74b49216c217d083ae8ec4e7e
schlingelkind <no@email.here>
2026-06-06 11:46:02 +0200

    Writing Header in STRUCT works

--- src/wav_header.cpp
+++ src/wav_header.cpp
@@ -1,70 +1,95 @@
 /* 2026-06-05
- * reading/writing a wav header */
+ * writing a wav header and rectangle function */
 
 
 #include <stdio.h>
 #include <stdlib.h>
 using namespace std;
 
+#define FILENAME "../output.wav"
+
+int WriteHeaderToFile(FILE* file);
+
+const int ChunkID = /*0x52494646;*/0x46464952; //0; 4 bytes, "RIFF" (ASCII)
+const int Format = /*0x57415645;/*/0x45564157; //(8; 4 bytes, "WAVE" (ASCII)
+const int Subchunk1ID = /* 0x666d7420 ;*/ 0x20746d66; //12; 4 bytes "fmt " (ASCII)
+const int Subchunk1Size = 16; //16; 4 bytes, PCM -> 16
+const short int AudioFormat = 1; //20; 2 bytes, PCM = 1
+const short int NumChannels = 1; //22; 2 bytes - should be user inputable
+const int SampleRate = 48000; //24; 4 bytes - should be user inputable
+const int ByteRate = SampleRate * NumChannels; //28; 4 bytes
+const short int BitsPerSample = 16; //34; 2 bytes, should be user inputable
+const short int BlockAlign = NumChannels * (BitsPerSample/8); //32; 2 bytes
+const int Subchunk2ID = /*0x64617461;*/ 0x61746164; //36; 4 bytes, "data" (ASCII)
+			      
+const float LengthSeconds = 0.001; //Not Part of WAV-header, should be inputable.			
+const int NumSamples = SampleRate * LengthSeconds;
+
+const int Subchunk2Size = NumSamples * NumChannels * (BitsPerSample/8); //40; 4 bytes -- REMEMBER TO SET LATER IF NEEDED
+const int ChunkSize = 36 + Subchunk2Size; //4; 4 bytes
+
+//not part of wav header!
+int FrequencyHertz = 1000;
+int SamplesPerWaveCycle = SampleRate/FrequencyHertz;
+int SamplesPerHalfWaveCycle = SamplesPerWaveCycle/2;
+//printf("whole: %d, half: %d", SamplesPerWaveCycle, SamplesPerHalfWaveCycle);
+
+typedef struct
+{
+	int ChunkID; // = ChunkID;
+	int ChunkSize; // = ChunkSize;
+	int Format; // = Format;
+	int Subchunk1ID; //= Subchunk1ID;
+	int Subchunk1Size; // = Subchunk1Size;
+	short int AudioFormat; // = AudioFormat;
+	short int NumChannels;// = NumChannels; 
+	int SampleRate;// = SampleRate;
+	int ByteRate;// = ByteRate;
+	short BlockAlign;// = BlockAlign;
+	short BitsPerSample;// = BitsPerSample;
+	int Subchunk2ID;// = Subchunk2ID;
+	int Subchunk2Size;// = Subchunk2Size;
+} WavHeader;
+
+WavHeader header = 
+	{
+		ChunkID,
+		ChunkSize,
+		Format,
+		Subchunk1ID,
+		Subchunk1Size,
+		AudioFormat,
+		NumChannels,
+		SampleRate,
+		ByteRate,
+		BlockAlign,
+		BitsPerSample,
+		Subchunk2ID,
+		Subchunk2Size
+	};
+
+
 int main(int argc, char** argv)
 {
 	//offset; size, content, comments -- BEWARE OF ORDER, SEE OFFSET!
 	
-	const int ChunkID = /*0x52494646;*/0x46464952; //0; 4 bytes, "RIFF" (ASCII)
-	const int Format = /*0x57415645;/*/0x45564157; //(8; 4 bytes, "WAVE" (ASCII)
-	const int Subchunk1ID = /* 0x666d7420 ;*/ 0x20746d66; //12; 4 bytes "fmt " (ASCII)
-	const int Subchunk1Size = 16; //16; 4 bytes, PCM -> 16
-	const short int AudioFormat = 1; //20; 2 bytes, PCM = 1
-	const short int NumChannels = 1; //22; 2 bytes - should be user inputable
-	const int SampleRate = 48000; //24; 4 bytes - should be user inputable
-	const int ByteRate = SampleRate * NumChannels; //28; 4 bytes
-	const short int BitsPerSample = 16; //34; 2 bytes, should be user inputable
-	const short int BlockAlign = NumChannels * (BitsPerSample/8); //32; 2 bytes
-	const int Subchunk2ID = /*0x64617461;*/ 0x61746164; //36; 4 bytes, "data" (ASCII)
-				      
-	const int LengthSeconds = 10; //Not Part of WAV-header, should be inputable.			
-	const int NumSamples = SampleRate * LengthSeconds;
-
-	const int Subchunk2Size = NumSamples * NumChannels * (BitsPerSample/8); //40; 4 bytes -- REMEMBER TO SET LATER IF NEEDED
-	const int ChunkSize = 36 + Subchunk2Size; //4; 4 bytes
-
-	//not part of wav header!
-	int FrequencyHertz = 1000;
-	int SamplesPerWaveCycle = SampleRate/FrequencyHertz;
-	int SamplesPerHalfWaveCycle = SamplesPerWaveCycle/2;
-	//printf("whole: %d, half: %d", SamplesPerWaveCycle, SamplesPerHalfWaveCycle);
 
 
 	//create file:
 	FILE* file;
-	file = fopen("../output.wav", "wb");
+	file = fopen(FILENAME, "wb");
 	
-
-	//initialize file
-	fwrite(&ChunkID, sizeof(ChunkID), 1, file);
+	//writing struct to file
+	WriteHeaderToFile(file);
 	fclose(file);
-	file = fopen("../output.wav", "ab");
 
-
-	//append rest of header
-	fwrite(&ChunkSize, sizeof(ChunkSize), 1, file);
-	fwrite(&Format, sizeof(Format), 1, file);
-	fwrite(&Subchunk1ID, sizeof(Subchunk1ID), 1, file);
-	fwrite(&Subchunk1Size, sizeof(Subchunk1Size), 1, file);
-	fwrite(&AudioFormat, sizeof(AudioFormat), 1, file);
-	fwrite(&NumChannels, sizeof(NumChannels), 1, file);
-	fwrite(&SampleRate, sizeof(SampleRate), 1, file);
-	fwrite(&ByteRate, sizeof(ByteRate), 1, file);
-	fwrite(&BlockAlign, sizeof(BlockAlign), 1, file);
-	fwrite(&BitsPerSample, sizeof(BitsPerSample), 1, file);
-	fwrite(&Subchunk2ID, sizeof(Subchunk2ID), 1, file);
-	fwrite(&Subchunk2Size, sizeof(Subchunk2Size), 1, file);
-	
 	//writing the function to the file
+	file = fopen(FILENAME, "ab");
 	short int sample = 0; //two bytes (See above!)
 	for(int i = 0; i < NumSamples; i++)
 	{
-		sample = (int)(0x7FFF*0.8) * ((i % SamplesPerWaveCycle) + 1 <= SamplesPerHalfWaveCycle) + (int)(0x7FFF*-0.8) * ((i % SamplesPerWaveCycle) + 1 > SamplesPerHalfWaveCycle);
+		sample = (int)(0x7FFF * 0.8) * (i % SamplesPerWaveCycle + 1 <= SamplesPerHalfWaveCycle)
+			+ (int)(0x7FFF * -0.8) * (i % SamplesPerWaveCycle + 1 > SamplesPerHalfWaveCycle);
 		fwrite(&sample,	sizeof(sample), 1, file); 
 	}
 
@@ -72,5 +97,11 @@ int main(int argc, char** argv)
 	//fprintf(file, "%c", EOF);
 	fclose(file);
 
+	return 0;
+}
+
+int WriteHeaderToFile(FILE* file)
+{
+	fwrite(&header, sizeof(WavHeader), 1, file);
 	return 0;
 }
